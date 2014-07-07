48d781a541d4a798c91fa27c5a402152427ddd25
Александр <@10.10.10.38>
2014-07-07 10:23:20 +0400

    Рефакторинг структуры.

---
+++ DC_PharmaCRM/src/ru/dreamcloud/pharmcrm/entities/Patient.java
@@ -0,0 +1,13 @@
+package ru.dreamcloud.pharmcrm.entities;
+
+public class Patient {
+	
+	Integer patientId;
+	String firstName;
+	String lastName;
+	String middleName;
+	Integer age;
+	Integer diagnosisType;
+	Integer resolutionType;	
+
+}
